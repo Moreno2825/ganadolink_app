754479cf1f081691f1f7837c6b6fd83222d4d099
Moreno2825 <rodrigocaraveo14@gmail.com>
2024-06-19 21:12:43 -0600

    fix(); listview

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:ganadolink_app/utils/constant.dart';
 import 'package:ganadolink_app/utils/responsive.dart';
 import 'package:parallax_cards/parallax_cards.dart';
+// import 'package:parallax_cards/parallax_cards.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -18,6 +20,13 @@ final images = [
   {'image': 'assets/images/vaca_3.png'},
 ];
 
+final List<Map<String, String>> people = [
+  {'firstName': 'John', 'lastName': 'Doe'},
+  {'firstName': 'Jane', 'lastName': 'Smith'},
+  {'firstName': 'Alex', 'lastName': 'Johnson'},
+  // Agrega más personas aquí si es necesario
+];
+
 final imagePaths = images.map((imageMap) => imageMap['image']!).toList();
 
 class _HomeScreenState extends State<HomeScreen> {
@@ -32,71 +41,231 @@ class _HomeScreenState extends State<HomeScreen> {
           FocusScope.of(context).unfocus();
         },
         child: Container(
-          height: resp.height,
-          width: resp.width,
-          padding: const EdgeInsets.only(top: 20, right: 8, left: 8),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.only(top: 20, right: 0, left: 20),
           child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 26),
-                child: SizedBox(
-                  height: 85,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: RichText(
-                          maxLines: 2,
-                          text: const TextSpan(
-                            children: [
-                              TextSpan(
-                                text: 'Gestiona tu ',
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Poppins'),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 26),
+                      child: SizedBox(
+                        height: 85,
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: RichText(
+                                maxLines: 2,
+                                text: const TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: 'Gestiona tu ',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'Poppins'),
+                                    ),
+                                    TextSpan(
+                                      text: 'ganado ',
+                                      style: TextStyle(
+                                          color: Color(0xffF27D16),
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'Poppins'),
+                                    ),
+                                    TextSpan(
+                                      text: 'con un ',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'Poppins'),
+                                    ),
+                                    TextSpan(
+                                      text: 'toque de innovación',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'Poppins'),
+                                    ),
+                                  ],
+                                ),
                               ),
-                              TextSpan(
-                                text: 'ganado ',
-                                style: TextStyle(
-                                    color: kPrimaryColor,
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Poppins'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    // Otros widgets dentro del ListView principal
+                    ParallaxCards(
+                      imagesList: imagePaths,
+                      scrollDirection: Axis.horizontal,
+                      width: 300,
+                      height: 180,
+                      onTap: (index) {},
+                      margin: const EdgeInsets.all(8),
+                    ),
+                    const SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Container(
+                          width: 141,
+                          height: 42,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffFDE5D0),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Crear Formulario',
+                              style: TextStyle(
+                                color: Color(0xffF27D16),
                               ),
-                              TextSpan(
-                                text: 'con un ',
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Poppins'),
-                              ),
-                              TextSpan(
-                                text: 'toque de innovación',
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Poppins'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Text(
+                          'ESTADÍSTICAS',
+                          style: TextStyle(
+                              color: Color(0xff434242), fontFamily: 'Poppins', fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 170,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffDBE4F2),
+                            borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black
+                                    .withOpacity(0.2), // Color de la sombra con opacidad
+                                spreadRadius: 0.5, // Propagación de la sombra
+                                blurRadius: 6, // Radio de desenfoque de la sombra
+                                offset: const Offset(2, 4), // Desplazamiento de la sombra (x, y)
                               ),
                             ],
                           ),
                         ),
+                        const SizedBox(width: 16), // Espacio entre los contenedores
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: const Color(0xffDBE4F2),
+                                  borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black
+                                          .withOpacity(0.2), // Color de la sombra con opacidad
+                                      spreadRadius: 0.5, // Propagación de la sombra
+                                      blurRadius: 6, // Radio de desenfoque de la sombra
+                                      offset:
+                                          const Offset(2, 4), // Desplazamiento de la sombra (x, y)
+                                    ),
+                                  ],
+                                ),
+                                width: 152,
+                                height: 90,
+                              ),
+                              const SizedBox(height: 16), // Espacio entre los contenedores
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: const Color(0xffDBE4F2),
+                                  borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black
+                                          .withOpacity(0.2), // Color de la sombra con opacidad
+                                      spreadRadius: 0.5, // Propagación de la sombra
+                                      blurRadius: 6, // Radio de desenfoque de la sombra
+                                      offset:
+                                          const Offset(2, 4), // Desplazamiento de la sombra (x, y)
+                                    ),
+                                  ],
+                                ),
+                                width: 152,
+                                height: 90,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 0, right: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            'REGISTROS MÁS RECIENTES',
+                            style: TextStyle(
+                                color: Color(0xff434242), fontFamily: 'Poppins', fontSize: 18),
+                          ),
+                          Text(
+                            'Ver más',
+                            style: TextStyle(
+                                color: Color(0xff434242), fontFamily: 'Poppins', fontSize: 18),
+                          )
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      height: 400, // Altura fija para el ListView.builder
+                      child: ListView.builder(
+                        itemCount: people.length,
+                        itemBuilder: (context, i) {
+                          return Container(
+                            height: 120,
+                            padding: const EdgeInsets.all(10),
+                            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                            decoration: BoxDecoration(
+                              color: Color(0xffF1F1F1),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Nombre: ${people[i]['firstName']}',
+                                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  'Apellido: ${people[i]['lastName']}',
+                                  style: const TextStyle(fontSize: 18),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              ParallaxCards(
-                imagesList: imagePaths,
-                scrollDirection: Axis.horizontal,
-                width: 300,
-                height: 180,
-                onTap: (index) {},
-                margin: const EdgeInsets.all(8),
-              )
             ],
           ),
         ),
