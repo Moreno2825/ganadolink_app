d070b159fc02b9b46158b3485e66c80d520b3528
PAULINA-MAY <feripau230@gmail.com>
2024-07-03 12:58:53 -0600

    The Cataloge Page has been changed

--- lib/components/screens/catalogue_screen.dart
+++ lib/components/screens/catalogue_screen.dart
@@ -1,19 +1,134 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-class CatalogueScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:ganadolink_app/utils/responsive.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CatalogueScreen extends StatefulWidget {
   const CatalogueScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-    
-      body:
-       Center(
-          child:  Text(
-              'Catalogue'
+  State<CatalogueScreen> createState() => _CatalogueScreenState();
+}
 
+class _CatalogueScreenState extends State<CatalogueScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+        Responsive resp = Responsive(context);
+    double myWidth = (resp.widthPercent(85));
+      
+    return  Scaffold(
+             appBar: AppBar(
+              
+              title: Text("Catalogue",
+              style: TextStyle(
+                fontWeight: FontWeight.bold, 
+              ),),
+            
+              centerTitle: true,
+             ),
+    
+      body: ScrollConfiguration(behavior:  ScrollConfiguration.of(context).copyWith(
+           dragDevices: {
+            PointerDeviceKind.touch,
+            PointerDeviceKind.mouse,
+          },
+      ), child: GestureDetector(
+            onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+            child: Container(
+            height: resp.height,
+            width: resp.width,
+            padding: const EdgeInsets.all(24),
+              child: Column(
+
+                      mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                   Expanded(
+              flex: 4, // Ocupa 2/5 del espacio disponible en el Row
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                     color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      SizedBox(width: 10),
+                      Icon( 
+                         size: 15,
+                         
+                        FontAwesomeIcons.search, color: Colors.grey),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          style: TextStyle(color: Colors.grey),
+                          decoration: InputDecoration(
+                            hintText: 'Buscar...',
+                            hintStyle: TextStyle(color: Colors.grey.withOpacity(0.7)),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-        ),
+            SizedBox(width: 10),
+            // Contenedor 2
+            Expanded(
+              flex: 1, // Ocupa 1/5 del espacio disponible en el Row
+              child: Container(
+                decoration: BoxDecoration(
+                     color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(10)
+
+                ),
+                height: 50,
+              
+                child: Center(
+                  child:  Icon( 
+                         size: 25,
+                         
+                        FontAwesomeIcons.qrcode, color: Colors.blue),
+                ),
+              ),
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              flex: 1, // Ocupa 1/5 del espacio disponible en el Row
+              child: Container(
+                decoration: BoxDecoration(
+                     color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(10)
+
+                ),
+                height: 50,
+              
+                child: Center(
+                  child:  Icon( 
+                         size: 25,
+                         
+                        FontAwesomeIcons.download, color: Colors.blue),
+                ),
+              ),
+            ),
+
+                        ],
+                      )
+                    ],
+
+              ),
+            )
+
+      ))
         
       );
   }
