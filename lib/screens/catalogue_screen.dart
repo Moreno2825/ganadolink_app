e16f601a00e263b8efea06327e424f2ede306378
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2024-07-03 23:45:48 -0600

    fix(); correccion de codigo

--- lib/screens/catalogue_screen.dart
+++ lib/screens/catalogue_screen.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:ganadolink_app/utils/responsive.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ganadolink_app/widgets/listview_widget.dart';
 
 class CatalogueScreen extends StatefulWidget {
   const CatalogueScreen({super.key});
@@ -12,124 +13,116 @@ class CatalogueScreen extends StatefulWidget {
 }
 
 class _CatalogueScreenState extends State<CatalogueScreen> {
-
-
   @override
   Widget build(BuildContext context) {
-        Responsive resp = Responsive(context);
+    Responsive resp = Responsive(context);
     double myWidth = (resp.widthPercent(85));
-      
-    return  Scaffold(
-             appBar: AppBar(
-              
-              title: Text("Catalogue",
-              style: TextStyle(
-                fontWeight: FontWeight.bold, 
-              ),),
-            
-              centerTitle: true,
-             ),
-    
-      body: ScrollConfiguration(behavior:  ScrollConfiguration.of(context).copyWith(
-           dragDevices: {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Catalogue",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(
+          dragDevices: {
             PointerDeviceKind.touch,
             PointerDeviceKind.mouse,
           },
-      ), child: GestureDetector(
-            onTap: () {
+        ),
+        child: GestureDetector(
+          onTap: () {
             FocusScope.of(context).unfocus();
           },
-            child: Container(
+          child: Container(
             height: resp.height,
             width: resp.width,
             padding: const EdgeInsets.all(24),
-              child: Column(
-
-                      mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                   Expanded(
-              flex: 4, // Ocupa 2/5 del espacio disponible en el Row
-              child: Container(
-                height: 50,
-                decoration: BoxDecoration(
-                     color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(10)
-                ),
-                child: Center(
-                  child: Row(
-                    children: [
-                      SizedBox(width: 10),
-                      Icon( 
-                         size: 15,
-                         
-                        FontAwesomeIcons.search, color: Colors.grey),
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: TextField(
-                          style: TextStyle(color: Colors.grey),
-                          decoration: InputDecoration(
-                            hintText: 'Buscar...',
-                            hintStyle: TextStyle(color: Colors.grey.withOpacity(0.7)),
-                            border: InputBorder.none,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 4, // Ocupa 2/5 del espacio disponible en el Row
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Center(
+                          child: Row(
+                            children: [
+                              const SizedBox(width: 10),
+                              const Icon(
+                                  size: 15,
+                                  FontAwesomeIcons.search,
+                                  color: Colors.grey),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: TextField(
+                                  style: const TextStyle(color: Colors.grey),
+                                  decoration: InputDecoration(
+                                    hintText: 'Buscar...',
+                                    hintStyle: TextStyle(
+                                        color: Colors.grey.withOpacity(0.7)),
+                                    border: InputBorder.none,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(width: 10),
+                    // Contenedor 2
+                    Expanded(
+                      flex: 1, // Ocupa 1/5 del espacio disponible en el Row
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10)),
+                        height: 50,
+                        child: const Center(
+                          child: Icon(
+                              size: 25,
+                              FontAwesomeIcons.qrcode,
+                              color: Colors.blue),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      flex: 1, // Ocupa 1/5 del espacio disponible en el Row
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10)),
+                        height: 50,
+                        child: const Center(
+                          child: Icon(
+                              size: 25,
+                              FontAwesomeIcons.download,
+                              color: Colors.blue),
+                        ),
+                      ),
+                    ),
+
+                   
+                  ],
                 ),
-              ),
+                 const CustomListview()
+              ],
             ),
-            SizedBox(width: 10),
-            // Contenedor 2
-            Expanded(
-              flex: 1, // Ocupa 1/5 del espacio disponible en el Row
-              child: Container(
-                decoration: BoxDecoration(
-                     color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(10)
-
-                ),
-                height: 50,
-              
-                child: Center(
-                  child:  Icon( 
-                         size: 25,
-                         
-                        FontAwesomeIcons.qrcode, color: Colors.blue),
-                ),
-              ),
-            ),
-            SizedBox(width: 10),
-            Expanded(
-              flex: 1, // Ocupa 1/5 del espacio disponible en el Row
-              child: Container(
-                decoration: BoxDecoration(
-                     color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(10)
-
-                ),
-                height: 50,
-              
-                child: Center(
-                  child:  Icon( 
-                         size: 25,
-                         
-                        FontAwesomeIcons.download, color: Colors.blue),
-                ),
-              ),
-            ),
-
-                        ],
-                      )
-                    ],
-
-              ),
-            )
-
-      ))
-        
-      );
+          ),
+        ),
+      ),
+    );
   }
 }
